f916aa5fac000771e0e5515ecfd1afaf66d545ac
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-24 14:02:08 -0400

    testing de i18

--- test/unit/i18n.test.js
+++ test/unit/i18n.test.js
@@ -1,11 +1,31 @@
-import { changeLanguage, translate } from '../../src/components/i18n';
+/**
+ * @jest-environment jsdom
+ */
+
+import  '../../src/components/i18n';
 import en from '../../src/langs/en.json';
 import es from '../../src/langs/es.json';
+import i18n from "i18next";
 
-test('translates text based on selected language', () => {
-    changeLanguage('en');
-    expect(translate('welcome')).toBe(en.welcome);
+describe('i18n configuration', () => {
+    beforeAll(() => {
+        // Inicializar i18n antes de cada prueba
+        return i18n.init({
+            resources: {
+                en: { translation: en },
+                es: { translation: es },
+            },
+            fallbackLng: 'en',
+            interpolation: { escapeValue: false },
+            detection: { order: ['localStorage', 'navigator'], caches: ['localStorage'] },
+        });
+    });
 
-    changeLanguage('es');
-    expect(translate('welcome')).toBe(es.welcome);
+    it('should translate text based on selected language', () => {
+        i18n.changeLanguage('en');
+        expect(i18n.t('app_title')).toBe(en.app_title);
+
+        i18n.changeLanguage('es');
+        expect(i18n.t('app_title')).toBe(es.app_title);
+    });
 });
