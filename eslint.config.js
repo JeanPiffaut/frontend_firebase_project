0ec97a2c30ec396d36f12fed9bcd0068e08b2bcd
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-24 01:57:24 -0400

    configuracion de testing

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+const js = require('@eslint/js');
+const pluginNode = require('eslint-plugin-node');
+
+module.exports = [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                jest: 'readonly',
+                require: 'readonly',
+                module: 'readonly',
+                process: 'readonly',
+                console: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
+                describe: 'readonly',
+                it: 'readonly',
+                expect: 'readonly',
+            },
+        },
+        plugins: {
+            node: pluginNode,
+        },
+        rules: {
+            indent: ['error', 4],
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            'no-trailing-spaces': 'error',
+            eqeqeq: 'error',
+            'no-unused-vars': 'warn',
+            'no-console': 'warn',
+            'consistent-return': 'error',
+            'no-undef': 'error',
+            'no-redeclare': 'error',
+            'no-dupe-keys': 'error',
+        },
+    },
+    {
+        files: ['**/*.js'],
+        ignores: ['node_modules/**', 'build/**']
+    },
+];
