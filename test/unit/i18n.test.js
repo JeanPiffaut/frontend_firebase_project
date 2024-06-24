9b28abc257aeeda95474e693a509d4fe46b404c3
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-24 14:04:38 -0400

    testing de i18 sin configuracion en el testing, si no que con la configuracion del ambiente

--- test/unit/i18n.test.js
+++ test/unit/i18n.test.js
@@ -8,19 +8,6 @@ import es from '../../src/langs/es.json';
 import i18n from "i18next";
 
 describe('i18n configuration', () => {
-    beforeAll(() => {
-        // Inicializar i18n antes de cada prueba
-        return i18n.init({
-            resources: {
-                en: { translation: en },
-                es: { translation: es },
-            },
-            fallbackLng: 'en',
-            interpolation: { escapeValue: false },
-            detection: { order: ['localStorage', 'navigator'], caches: ['localStorage'] },
-        });
-    });
-
     it('should translate text based on selected language', () => {
         i18n.changeLanguage('en');
         expect(i18n.t('app_title')).toBe(en.app_title);
