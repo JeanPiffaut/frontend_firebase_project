bae030e3a2d9410029670c44fc955d05a89685e3
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-07-08 12:39:47 -0400

    correccion de test:integration

--- test/__mocks__/fileTransformer.js
+++ test/__mocks__/fileTransformer.js
@@ -1,8 +1,9 @@
-// test/__mocks__/fileTransformer.js
 const path = require('path');
 
 module.exports = {
-    process(src, filename, config, options) {
-        return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+    process(src, filename) {
+        return {
+            code: `module.exports = ${JSON.stringify(path.basename(filename))};`,
+        };
     },
 };
