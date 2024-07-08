6bcc6472543fdc907d518a8ed34c6031fe5b3f7a
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-07-08 11:11:49 -0400

    ejecucion de test de integracion completa

--- test/__mocks__/fileMock.js
+++
@@ -1,2 +0,0 @@
-// test/__mocks__/fileMock.js
-module.exports = 'test-file-stub';
--- test/integration/App.test.js
+++ test/integration/App.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../../src/App';
 
-test('renders Home component on / route', () => {
+test('renders the Home component when the path is "/"', () => {
     render(
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     );
 
-    expect(screen.getByText('Learn React')).toBeInTheDocument();
+    // Verifica que el componente Home está presente en el DOM
+    const homeElement = screen.getByText(/welcome to home/i); // Ajusta esto según el contenido real de Home
+    expect(homeElement).toBeInTheDocument();
 });
