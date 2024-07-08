317256ba368446acfe2de22f825a6d5ff8c2a09f
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-07-08 11:59:58 -0400

    testing App correcto

--- test/integration/App.test.js
+++ test/integration/App.test.js
@@ -1,15 +1,18 @@
+// test/integration/App.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import '../../src/components/i18n';
 import App from '../../src/App';
 import { HelmetProvider } from 'react-helmet-async';
 
 test('renders the Home component when the path is "/"', () => {
     render(
-        <App />
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
     );
 
     // Verifica que el componente Home está presente en el DOM
-    const homeElement = screen.getByText('Home'); // Ajusta esto según el contenido real de Home
-    expect(homeElement).toBeInTheDocument();
+    const editSaveText = screen.getByText(/edit/i); // Busca el texto "Edit" que proviene de la traducción
+    expect(editSaveText).toBeInTheDocument();
 });
